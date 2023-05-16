ac72f07d131dd9002ba23180761bbc12cbbd7f45
AIM1rage <michael050100@gmail.com>
2023-05-16 22:49:15 +0500

    Переписал парсер на класс

--- parser.py
+++ parser.py
@@ -4,95 +4,101 @@ from constants import *
 import re
 
 
-def parse(expression: str) -> Poly:
-    expression_without_spaces = str(re.sub('\\s', '', expression))
-    return calculate_postfix(to_postfix(expression_without_spaces))
+class Parser:
+    @staticmethod
+    def parse(expression: str) -> Poly:
+        expression_without_spaces = str(re.sub('\\s', '', expression))
+        return Parser.calculate_postfix(
+            Parser.to_postfix(expression_without_spaces))
 
+    @staticmethod
+    def calculate_postfix(postfix):
+        stack = list()
+        for token in postfix:
+            if type(token) is int:
+                stack.append(Poly([token]))
+            elif token == 'x':
+                stack.append(Poly([1, 0]))
+            elif token in operators:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+                Parser.check_operation_correctness(operators[token], operand1,
+                                                   operand2)
+                stack.append(operators[token](operand1, operand2))
+        return stack.pop()
 
-def calculate_postfix(postfix):
-    stack = list()
-    for token in postfix:
-        if type(token) is int:
-            stack.append(Poly([token]))
-        elif token == 'x':
-            stack.append(Poly([1, 0]))
-        elif token in operators:
-            operand2 = stack.pop()
-            operand1 = stack.pop()
-            check_operation_correctness(operators[token], operand1, operand2)
-            stack.append(operators[token](operand1, operand2))
-    return stack.pop()
-
-
-def to_postfix(expression):
-    postfix = list()
-    stack = list()
-    index = 0
-    while index < len(expression):
-        number_result = read_number(expression, index)
-        x_result = read_symbol(expression, index, read_predicates['x'])
-        operator_result = read_symbol(
-            expression, index, read_predicates['operator'])
-        open_bracket_result = read_symbol(
-            expression, index, read_predicates['open bracket'])
-        close_bracket_result = read_symbol(
-            expression, index, read_predicates['close bracket'])
-        if number_result:
-            postfix.append(number_result[0])
-            index = number_result[1]
-        elif x_result:
-            postfix.append(x_result[0])
-            index = x_result[1]
-        elif operator_result:
-            while len(stack) > 0 and \
-                    stack[-1] in operators.keys() and \
-                    operator_priorities[stack[-1]] >= \
-                    operator_priorities[operator_result[0]]:
-                postfix.append(stack.pop())
-            stack.append(operator_result[0])
-            index = operator_result[1]
-        elif open_bracket_result:
-            stack.append(open_bracket_result[0])
-            index = open_bracket_result[1]
-        elif close_bracket_result:
-            while len(stack) > 0 and stack[-1] not in brackets.keys():
-                postfix.append(stack.pop())
-            if len(stack) == 0 or \
-                    brackets[stack.pop()] != close_bracket_result[0]:
+    @staticmethod
+    def to_postfix(expression):
+        postfix = list()
+        stack = list()
+        index = 0
+        while index < len(expression):
+            number_result = Parser.read_number(
+                expression, index)
+            x_result = Parser.read_symbol(
+                expression, index, read_predicates['x'])
+            operator_result = Parser.read_symbol(
+                expression, index, read_predicates['operator'])
+            open_bracket_result = Parser.read_symbol(
+                expression, index, read_predicates['open bracket'])
+            close_bracket_result = Parser.read_symbol(
+                expression, index, read_predicates['close bracket'])
+            if number_result:
+                postfix.append(number_result[0])
+                index = number_result[1]
+            elif x_result:
+                postfix.append(x_result[0])
+                index = x_result[1]
+            elif operator_result:
+                while len(stack) > 0 and \
+                        stack[-1] in operators.keys() and \
+                        operator_priorities[stack[-1]] >= \
+                        operator_priorities[operator_result[0]]:
+                    postfix.append(stack.pop())
+                stack.append(operator_result[0])
+                index = operator_result[1]
+            elif open_bracket_result:
+                stack.append(open_bracket_result[0])
+                index = open_bracket_result[1]
+            elif close_bracket_result:
+                while len(stack) > 0 and stack[-1] not in brackets.keys():
+                    postfix.append(stack.pop())
+                if len(stack) == 0 or \
+                        brackets[stack.pop()] != close_bracket_result[0]:
+                    raise ArithmeticError
+                index = close_bracket_result[1]
+            else:
                 raise ArithmeticError
-            index = close_bracket_result[1]
-        else:
-            raise ArithmeticError
-    postfix.extend(reversed(stack))
-    return postfix
+        postfix.extend(reversed(stack))
+        return postfix
 
+    # пытается прочитать число и возвращает (number, index),
+    # если не получилось - возвращает None
+    @staticmethod
+    def read_number(expression, index):
+        digits = list()
+        while index < len(expression) and str(expression[index]).isdigit():
+            digits.append(expression[index])
+            index += 1
+        if digits:
+            return int(''.join(digits)), index
+        return None
 
-# пытается прочитать число и возвращает (number, index),
-# если не получилось - возвращает None
-def read_number(expression, index):
-    digits = list()
-    while index < len(expression) and str(expression[index]).isdigit():
-        digits.append(expression[index])
-        index += 1
-    if digits:
-        return int(''.join(digits)), index
-    return None
+    # пытается прочитать символ и возвращает (symbol, index),
+    # если не получилось - возвращает None
+    @staticmethod
+    def read_symbol(expression, index, predicate):
+        if index < len(expression) and predicate(expression[index]):
+            index += 1
+            return expression[index - 1], index
+        return None
 
-
-# пытается прочитать символ и возвращает (symbol, index),
-# если не получилось - возвращает None
-def read_symbol(expression, index, predicate):
-    if index < len(expression) and predicate(expression[index]):
-        index += 1
-        return expression[index - 1], index
-    return None
-
-
-def check_operation_correctness(operator_to_check, operand1, operand2):
-    # TODO
-    return True
+    @staticmethod
+    def check_operation_correctness(operator_to_check, operand1, operand2):
+        # TODO
+        return True
 
 
 if __name__ == '__main__':
-    result = list(map(parse, example.to_parse))
+    result = list(map(Parser.parse, example.to_parse))
     pass
