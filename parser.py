786e996896bde30f6e5e5f89898068d15d2ec27f
AIM1rage <michael050100@gmail.com>
2023-06-23 23:00:02 +0500

    Добавил математическую форму (умножение на число без знака умножения, унарный минус)

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ class Parser:
     @staticmethod
     def parse(expression: str) -> Poly:
         if not expression:
-            raise ArithmeticError
+            raise ValueError('Пустая строка ввода!')
         expression_without_spaces = str(re.sub('\\s', '', expression))
         return Parser._calculate_postfix_(
             Parser._to_postfix_(expression_without_spaces))
@@ -16,30 +16,22 @@ class Parser:
     def _calculate_postfix_(postfix):
         stack = list()
         for token in postfix:
-            if type(token) is int:
-                stack.append(Poly([token]))
-            elif token == 'x':
-                stack.append(Poly([1, 0]))
+            if type(token) == Poly:
+                stack.append(token)
             elif token in operators:
-                if token == '-' and (len(stack) < 2 or stack[-2] in operators):
-                    operand2 = stack.pop()
-                    operand1 = Poly([0])
-                else:
-                    operand2 = stack.pop()
-                    operand1 = stack.pop()
-                Parser._check_operation_correctness_(operators[token],
-                                                     operand1,
-                                                     operand2)
+                operand2 = stack.pop()
+                operand1 = stack.pop()
                 stack.append(operators[token](operand1, operand2))
-        if len(stack) == 1:
-            return stack.pop()
-        raise ArithmeticError
+            else:
+                raise Exception(f'{token}')
+        return stack.pop()
 
     @staticmethod
     def _to_postfix_(expression):
         postfix = list()
         stack = list()
         index = 0
+        previous_token = None
         while index < len(expression):
             number_result = Parser._read_number_(
                 expression, index)
@@ -51,34 +43,64 @@ class Parser:
                 expression, index, read_predicates['open bracket'])
             close_bracket_result = Parser._read_symbol_(
                 expression, index, read_predicates['close bracket'])
-            if number_result:
-                postfix.append(number_result[0])
-                index = number_result[1]
-            elif x_result:
-                postfix.append(x_result[0])
-                index = x_result[1]
-            elif operator_result:
-                while len(stack) > 0 and \
-                        stack[-1] in operators.keys() and \
-                        operator_priorities[stack[-1]] >= \
-                        operator_priorities[operator_result[0]]:
-                    postfix.append(stack.pop())
-                stack.append(operator_result[0])
-                index = operator_result[1]
-            elif open_bracket_result:
-                stack.append(open_bracket_result[0])
-                index = open_bracket_result[1]
-            elif close_bracket_result:
-                while len(stack) > 0 and stack[-1] not in brackets.keys():
-                    postfix.append(stack.pop())
-                if len(stack) == 0 or \
-                        brackets[stack.pop()] != close_bracket_result[0]:
-                    raise ArithmeticError
-                index = close_bracket_result[1]
-            else:
-                raise ArithmeticError
+            previous_token, index = Parser._add_token_(number_result,
+                                                       x_result,
+                                                       operator_result,
+                                                       open_bracket_result,
+                                                       close_bracket_result,
+                                                       expression,
+                                                       postfix, stack, index,
+                                                       previous_token)
         postfix.extend(reversed(stack))
         return postfix
+
+    @staticmethod
+    def _add_token_(number_result, x_result, operator_result,
+                    open_bracket_result, close_bracket_result,
+                    expression, postfix, stack, index, previous_token):
+        if number_result or x_result:
+            if number_result:
+                token = Poly([number_result[0]])
+                postfix.append(token)
+                index = number_result[1]
+            else:
+                token = Poly([1, 0])
+                postfix.append(token)
+                index = x_result[1]
+            if type(previous_token) == Poly:
+                print('Yes!')
+                stack.append('*')
+        elif operator_result:
+            token = operator_result[0]
+            if previous_token in operators.keys():
+                raise ValueError(f'Двойной оператор! '
+                                 f'Не следует операторы '
+                                 f'\'{previous_token}\' и \'{token}\' подряд')
+            if token == '-' and (
+                    previous_token is None or previous_token == '('):
+                postfix.append(0)
+            while len(stack) > 0 and \
+                    stack[-1] in operators.keys() and \
+                    operator_priorities[stack[-1]] >= \
+                    operator_priorities[operator_result[0]]:
+                postfix.append(stack.pop())
+            stack.append(token)
+            index = operator_result[1]
+        elif open_bracket_result:
+            token = open_bracket_result[0]
+            stack.append(token)
+            index = open_bracket_result[1]
+        elif close_bracket_result:
+            token = close_bracket_result[0]
+            while len(stack) > 0 and stack[-1] not in brackets.keys():
+                postfix.append(stack.pop())
+            if len(stack) == 0 or \
+                    brackets[stack.pop()] != close_bracket_result[0]:
+                raise ValueError('Пропущена открывающая скобка!')
+            index = close_bracket_result[1]
+        else:
+            raise ValueError(f'Неизвестный символ \'{expression[index]}\'!')
+        return token, index
 
     # пытается прочитать число и возвращает (number, index),
     # если не получилось - возвращает None
@@ -100,8 +122,3 @@ class Parser:
             index += 1
             return expression[index - 1], index
         return None
-
-    @staticmethod
-    def _check_operation_correctness_(operator_to_check, operand1, operand2):
-        # TODO
-        return True
